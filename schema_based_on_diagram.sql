ca0612135c17a6f57b1dca965d9702986084f922
Felix Odete <felix.odette@gmail.com>
2022-07-27 11:18:39 +0300

    Medical histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,2 +1,22 @@
 -- Create database
 CREATE DATABASE clinic;
+
+-- Create patients table
+DROP TABLE IF EXISTS patients;
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
+
+-- Create medical histories table
+DROP TABLE IF EXISTS medical_histories;
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(100),
+  PRIMARY KEY(id),
+  FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
